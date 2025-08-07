cf5265c97fea0c2c5787c64f8d44b02ebe76dea9
Shamshad Ansari <shamshadansari43984@gmail.com>
2025-08-07 17:47:56 +0530

    Add files via upload

---
+++ Array/multipleArray.java
@@ -0,0 +1,18 @@
+package Array;
+
+public class multipleArray {
+    public static void main (String[] arge)
+    {
+            //declaring and initializing 2D array
+            int arr[][]={{1,2,3},{2,4,5},{5,5,4}};
+    //printing 2D array
+    for(int i=0;i<3;i++){
+        for(int j=0; j<3;j++){
+            System.out.println(arr[i][j] + "");
+        }
+        System.out.println();
+    }
+
+
+    }
+}
